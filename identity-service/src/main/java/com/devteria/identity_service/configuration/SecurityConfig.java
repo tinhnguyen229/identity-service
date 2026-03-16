2ee81d8e80ab4626be89a57a80c8818d723e6298
tinhnn229tb <tinhnn229@gmail.com>
2026-03-16 18:12:34 +0700

    <fix> jwt authentication

--- identity-service/src/main/java/com/devteria/identity_service/configuration/SecurityConfig.java
+++ identity-service/src/main/java/com/devteria/identity_service/configuration/SecurityConfig.java
@@ -1,19 +1,30 @@
 package com.devteria.identity_service.configuration;
 
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${jwt.signerKey}")
+    private String jwtSignerKey;
+
     private final String[] PUBLIC_ENDPOINTS = {
             "/v2/login",
             "/v1/introspect",
@@ -28,6 +39,19 @@ public class SecurityConfig {
         httpSecurity.csrf(
                 AbstractHttpConfigurer -> AbstractHttpConfigurer.disable()
         );
+        httpSecurity.oauth2ResourceServer(
+                oauth2 -> oauth2.jwt(
+                        jwtConfigurer -> jwtConfigurer.decoder(this.jwtDecoder())
+                )
+        );
         return httpSecurity.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(jwtSignerKey.getBytes(), "HS256");
+        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.
+                withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS256).build();
+        return jwtDecoder;
     }
 }
