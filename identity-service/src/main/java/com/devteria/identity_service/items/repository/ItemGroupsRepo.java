1dc798ab97c86fce5b83a0b7563dc6e3a9f554fe
tinhnn229tb <tinhnn229@gmail.com>
2026-03-19 09:51:00 +0700

    <fix> validate create Items

--- identity-service/src/main/java/com/devteria/identity_service/items/repository/ItemGroupsRepo.java
+++ identity-service/src/main/java/com/devteria/identity_service/items/repository/ItemGroupsRepo.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface ItemGroupsRepo extends JpaRepository<ItemGroups, Long> {
     List<ItemGroups> findByCategoryIdInAndIsDeleteFalse(List<Long> categoryIds);
+    ItemGroups findByIdAndIsDeleteFalse(Long id);
 }
--- identity-service/src/main/java/com/devteria/identity_service/items/service/ItemsService.java
+++ identity-service/src/main/java/com/devteria/identity_service/items/service/ItemsService.java
@@ -1,8 +1,11 @@
 package com.devteria.identity_service.items.service;
 
 
+import com.devteria.identity_service.exception.BusinessException;
 import com.devteria.identity_service.items.dto.ItemsCreateReq;
+import com.devteria.identity_service.items.entity.ItemGroups;
 import com.devteria.identity_service.items.entity.Items;
+import com.devteria.identity_service.items.repository.ItemGroupsRepo;
 import com.devteria.identity_service.items.repository.ItemsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,9 +14,16 @@ import org.springframework.stereotype.Service;
 public class ItemsService {
     @Autowired
     private ItemsRepo itemsRepo;
+    @Autowired
+    private ItemGroupsRepo itemGroupsRepo;
 
 
     public Items createItem(ItemsCreateReq request) {
+        ItemGroups group = itemGroupsRepo.findByIdAndIsDeleteFalse(request.getGroupId());
+        if (group == null) {
+            throw new RuntimeException("Không tồn tại groupId");
+        }
+
         Items items = Items
                 .builder()
                 .groupId(request.getGroupId())
